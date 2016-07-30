b764b4215360804008042ee3f36224eefedcb6a1
Пользователь <>
2016-07-30 20:03:38 +0300

    add role field to User

--- src/com/foodstore/serg/model/User.java
+++ src/com/foodstore/serg/model/User.java
@@ -1,5 +1,28 @@
 package com.foodstore.serg.model;
 
 public class User {
+	
+	private String login;
+	private String password;
+	private Role role;
+	
+	public User(String login, String password, Role role){
+		this.login = login;
+		this.password = password;
+		this.role = role;
+	}
+	
+	public String getLogin(){
+		return login;
+	}
+	
+	public String getPassword(){
+		return password;
+	}
+	
+	public Role getRole(){
+		return role;
+	}
+	
 
 }
