9d9ee56fd7b42602bbe560803427795798fef96a
Sergey <s.v.romankov@gmail.com>
2016-07-31 13:24:11 +0300

    add emailvalidator to check emails by registration

---
+++ src/com/foodstore/serg/utils/EmailValidator.java
@@ -0,0 +1,26 @@
+package com.foodstore.serg.utils;
+import java.util.regex.*;
+
+public class EmailValidator {
+	
+	
+	/**
+	 * Validate followings:
+	 * first part of email before @ can be 4 to 31 length, can contain
+	 * numbers and letters and also . (dot) and _ (underscore)
+	 * Part after @ can be at least 1 character long and first character
+	 * must be a letter
+	 * Part after . can be either 2 or 3 characters long
+	 * @param emailToCheck string contains email field
+	 * @return
+	 */
+	public static boolean validateEmail(String emailToCheck){
+		
+		final String MAIL_PATTERN = "^[A-Za-z0-9\\._]{4,31}+@[A-Za-z]{1}+[A-Za-z0-9]{0,}\\.[A-Za-z]{2,3}$";
+		
+		Pattern pattern = Pattern.compile(MAIL_PATTERN);
+		Matcher matcher = pattern.matcher(emailToCheck);
+		
+		return matcher.matches();
+	}
+}
